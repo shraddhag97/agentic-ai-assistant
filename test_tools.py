e251193a81c03383a4c769245e8e74393de89393
shraddhag97 <shraddha230897@gmail.com>
2026-02-23 11:06:41 +0530

    Removed test file

--- test_tools.py
+++
@@ -1,8 +0,0 @@
-from tools import search_web, extract_text_from_url
-
-links = search_web("AI in banking")
-print("Links:", links)
-
-if links:
-    text = extract_text_from_url(links[0])
-    print("\nExtracted text sample:\n", text[:500])
